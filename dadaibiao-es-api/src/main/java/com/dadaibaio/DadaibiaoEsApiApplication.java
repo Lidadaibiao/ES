c3bae81affb4d3a4b1002d5d1f94c59e056f3c8c
Lidadaibiao <945827167@qq.com>
2020-12-21 12:44:04 +0800

    es整合springboot 纺京东 纺织

---
+++ dadaibiao-es-api/src/main/java/com/dadaibaio/DadaibiaoEsApiApplication.java
@@ -0,0 +1,13 @@
+package com.dadaibaio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadaibiaoEsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadaibiaoEsApiApplication.class, args);
+	}
+
+}
---
+++ dadaibiao-es-api/src/test/java/com/dadaibaio/DadaibiaoEsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dadaibaio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadaibiaoEsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
